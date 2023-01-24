9cca0bcdd5e2c7c609229ba0d01ba507ebdfad0d
Gabriel <gabrielkelvinfreitas@gmail.com>
2023-01-24 16:52:41 -0300

    REQ4 - Finalizado, Title adicionado ao SolarSystem

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import PlanetCard from './components/PlanetCard';
 import SolarSystem from './components/SolarSystem';
 import Title from './components/Title';
+import Planets from './data/planets';
 
 class App extends React.Component {
   render() {
@@ -11,7 +12,10 @@ class App extends React.Component {
         <Header />
         <SolarSystem />
         <Title />
-        <PlanetCard />
+        { Planets.map(({ name, image }) => {
+          const e = <PlanetCard key={ name } planetName={ name } planetImage={ image } />;
+          return e;
+        })}
       </>
     );
   }
