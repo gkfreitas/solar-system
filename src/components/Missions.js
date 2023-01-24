bfe3b75ee065ab755931e61ba76e10540c1d6e9c
Gabriel <gabrielkelvinfreitas@gmail.com>
2023-01-24 17:11:18 -0300

    REQ10 - Finalizado, Missoes renderizadas

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 import Title from './Title';
 
 class Missions extends React.Component {
@@ -6,6 +8,17 @@ class Missions extends React.Component {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        {missions.map(({ name, year, country, destination }) => {
+          const e = (
+            <MissionCard
+              name={ name }
+              year={ year }
+              country={ country }
+              destination={ destination }
+            />
+          );
+          return e;
+        })}
       </div>
     );
   }
